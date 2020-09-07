79050927ea6276df68603b2673bca471f43a4a8d
marco varela <marco.varela12@gmail.com>
2020-09-07 22:49:42 +0100

    Working calculator

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import "./calculator.scss";
 
-const Display = ({ firstOperand, secondOperand, operator, total }) => {
-  return <div>{ total ? total : <>{firstOperand}{operator}{secondOperand}</>}</div>
+const Display = ({ operands, total }) => {
+  return <div className="display">{ total ? <>{operands} = {total}</> : operands}</div>
 }
 
 export default Display;
--- src/components/keypad.jsx
+++ src/components/keypad.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import Button from './button';
+import "./calculator.scss";
 
-const Keypad = ({ clear, selectOperand, selectOperator, result}) => {
+const Keypad = ({ clear, allClear, selectOperand, getResult }) => {
   return (
-      <div>
-        <div className="numbers">
-          <Button className="clear" onClick={clear} text="CE" />
-          <Button className="number" onClick={selectOperand} text="0" />
-          <Button className="number" onClick={selectOperand} text="1" />
-          <Button className="number" onClick={selectOperand} text="2" />
-          <Button className="number" onClick={selectOperand} text="3" />
-          <Button className="number" onClick={selectOperand} text="4" />
-          <Button className="number" onClick={selectOperand} text="5" />
-          <Button className="number" onClick={selectOperand} text="6" />
-          <Button className="number" onClick={selectOperand} text="7" />
-          <Button className="number" onClick={selectOperand} text="8" />
-          <Button className="number" onClick={selectOperand} text="9" />
-        </div>
-        <div className="operators">
-          <Button className="operator" onClick={selectOperator} text="+" />
-          <Button className="operator" onClick={selectOperator} text="-" />
-          <Button className="operator" onClick={selectOperator} text="×" />
-          <Button className="operator" onClick={selectOperator} text="÷" />
-          <Button className="operator" onClick={selectOperator} text="%" />
-          <Button className="total" onClick={result} text="=" />
-        </div>
+    <div className="keypad">
+      <div className="numbers">
+        <Button className="button" onClick={selectOperand} text="9" />
+        <Button className="button" onClick={selectOperand} text="8" />
+        <Button className="button" onClick={selectOperand} text="7" />
+        <Button className="button" onClick={selectOperand} text="6" />
+        <Button className="button" onClick={selectOperand} text="5" />
+        <Button className="button" onClick={selectOperand} text="4" />
+        <Button className="button" onClick={selectOperand} text="3" />
+        <Button className="button" onClick={selectOperand} text="2" />
+        <Button className="button" onClick={selectOperand} text="1" />
+        <Button className="button" onClick={selectOperand} text="0" />
+        <Button className="button" onClick={selectOperand} text="00" />
+        <Button className="button" onClick={getResult} text="=" />
       </div>
+      <div className="operators">
+        <Button className="button" onClick={selectOperand} text="^" />
+        <Button className="button" onClick={selectOperand} text="." />
+        <Button className="button" onClick={selectOperand} text="(" />
+        <Button className="button" onClick={selectOperand} text=")" />
+        <Button className="button" onClick={selectOperand} text="+" />
+        <Button className="button" onClick={selectOperand} text="-" />
+        <Button className="button" onClick={selectOperand} text="*" />
+        <Button className="button" onClick={selectOperand} text="/" />
+        <Button className="button" onClick={selectOperand} text="%" />
+        <Button className="button" onClick={clear} text="CE" />
+        <Button className="button" onClick={allClear} text="C" />
+      </div>
+    </div>
   );
 }
 
